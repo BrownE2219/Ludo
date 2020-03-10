f336dab1bcc247d2e674111457565f216b7a8cbb
OmAsati <58726381+OmAsati@users.noreply.github.com>
2020-03-10 20:15:05 +0530

    pawns can move

--- appludo.js
+++ appludo.js
@@ -1,14 +1,20 @@
-var activePlayer = Math.floor(Math.random()*4) + 1;
-document.querySelector('.p'+activePlayer+'1').style.display = "none";
-document.querySelector('.p'+activePlayer+'2').style.display = "block";
-document.querySelector('.p'+activePlayer+'3').style.display = "block";
-document.querySelector('.p'+activePlayer+'4').style.display = "block";
-document.querySelector('.p'+activePlayer+'5').style.display = "block";
-document.querySelector('.p'+activePlayer+'6').style.display = "block";
-document.querySelector('.p'+activePlayer+'7').style.display = "block";
-document.querySelector('.playroll'+activePlayer).addEventListener('click',roll);
+var activePlayer, dice, pos, but;
+var players;
+var dicerolled;
+var i = 1;
+init();
+/* player 0,1,2,3 dice 0,1,2,3 */
+document.querySelector('.playroll'+activePlayer).addEventListener('click',rolled);
+function rolled(){
+  roll();
+  dicerolled = true;
+/*  document.querySelector('.playroll'+activePlayer).removeEventListener('click',rolled); */
+  document.querySelector('.PlayerB'+activePlayer+'0').addEventListener('click',function(){pawn('0');});
+  document.querySelector('.PlayerB'+activePlayer+'1').addEventListener('click',function(){pawn('1');});
+  document.querySelector('.PlayerB'+activePlayer+'2').addEventListener('click',function(){pawn('2');});
+  document.querySelector('.PlayerB'+activePlayer+'3').addEventListener('click',function(){pawn('3');});
+}
 function roll(){
-  var dice;
   dice = Math.floor(Math.random()*6) + 1;
   switch(dice){
     case 1:
@@ -66,4 +72,49 @@ function roll(){
       document.querySelector('.p'+activePlayer+'7').style.display = "block";
       break;
   }
+}
+function init(){
+  activePlayer = 2;
+  dicerolled = false;
+  players = [0,1,2,3,0];
+  pos = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+document.querySelector('.p'+activePlayer+'1').style.display = "none";
+document.querySelector('.p'+activePlayer+'2').style.display = "block";
+document.querySelector('.p'+activePlayer+'3').style.display = "block";
+document.querySelector('.p'+activePlayer+'4').style.display = "block";
+document.querySelector('.p'+activePlayer+'5').style.display = "block";
+document.querySelector('.p'+activePlayer+'6').style.display = "block";
+document.querySelector('.p'+activePlayer+'7').style.display = "block";
+}
+function pawn(k){
+  var b = document.querySelector('.PlayerB'+activePlayer+k).style;
+  if(dicerolled)
+  {
+    if(activePlayer === 0)
+    {
+      b.left = document.querySelector('.g'+(1+dice)).style.left;
+      b.top = document.querySelector('.g'+(1+dice)).style.top;
+    }
+    if(activePlayer === 1)
+    {
+      b.left = document.querySelector('.r'+(1+dice)).style.left;
+      b.top = document.querySelector('.r'+(1+dice)).style.top;
+    }
+    if(activePlayer === 2)
+    {
+      b.left = document.querySelector('.b'+(1+dice)).style.left;
+      b.top = document.querySelector('.b'+(1+dice)).style.top;
+    }
+    if(activePlayer === 3)
+    {
+      b.left = document.querySelector('.y'+(1+dice)).style.left;
+      b.top = document.querySelector('.y'+(1+dice)).style.top;
+    }
+
+    dicerolled = false;
+  }
+}
+function nextplayer(){
+  activePlayer = players[players.findIndex(activePlayer)+1];
+  return activePlayer;
 }
